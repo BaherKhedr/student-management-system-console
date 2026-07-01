51c5094356f7e294bc148d56b1b21fa22fac1b7c
Baher <baherkhedr9@gmail.com>
2026-07-01 21:27:33 +0300

    Added AppDbContext and StudentConfiguration classes

---
+++ Config/StudentConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using ManagerApplicationSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerApplicationSystem.Config
+{
+    internal class StudentConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.Property(b => b.Id).ValueGeneratedNever();
+
+            builder.Property(b => b.Name).IsRequired().HasMaxLength(100);
+
+            builder.Property(b => b.Age).IsRequired();
+
+            builder.Property(b => b.Grade).IsRequired();
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using ManagerApplicationSystem.Config;
+using ManagerApplicationSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerApplicationSystem.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            var constr = configuration.GetSection("constr").Value;
+
+            optionsBuilder.UseSqlServer(constr);
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StudentConfiguration).Assembly);
+        }
+    }
+}
