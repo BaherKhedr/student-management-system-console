cfcba9fd808cb84a54a6c9a6f4e1ce6791c75b8f
Baher <baherkhedr9@gmail.com>
2026-07-04 14:46:11 +0300

    added factory class to create new appdbcontext for dependency injection

---
+++ Data/AppDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerApplicationSystem.Data
+{
+
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            var constr = config.GetSection("constr").Value;
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            optionsBuilder.UseSqlServer(constr);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
